768b1ebccfa8fa0a91a7156ed67f49751054e92f
Sergey Zhizhko <sergey.zhizhko@dataart.com>
2021-02-17 10:18:04 +0200

    add function references project

---
+++ FunctionReferences/EnumerableExtention.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleApp
+{
+    public static class EnumerableExtention
+    {
+        public static IEnumerable<T> Foreach<T>(this IEnumerable<T> collection, Action<T> action)
+        {
+            foreach (var item in collection)
+            {
+                action(item);
+                yield return item;
+            }
+        }
+
+        public static IEnumerable<T> OurWhere<T>(this IEnumerable<T> collection, Func<T, bool> checker)
+        {
+            foreach (T item in collection)
+            {
+                if (checker(item))
+                    yield return item;
+            }
+        }
+    }
+}
---
+++ FunctionReferences/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SampleApp;
+
+namespace FunctionReferences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new [] { 1, 2, 3, 4, 5 };
+
+
+            var checkedNumbers = numbers
+                .Foreach(Console.WriteLine)
+                .OurWhere(number => number > 3);
+
+            foreach (int number in checkedNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        public static IEnumerable<int> WhereEven(IEnumerable<int> collection)
+        {
+            foreach (int item in collection)
+            {
+                if (item % 2 == 0)
+                    yield return item;
+            }
+        }
+
+        public static IEnumerable<int> WherePositive(IEnumerable<int> collection)
+        {
+            foreach (int item in collection)
+            {
+                if (item > 0)
+                    yield return item;
+            }
+        }
+
+        public static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public static bool IsPositive(int number)
+        {
+            return number > 0;
+        }
+
+        private static string GetHelloMessage()
+        {
+            return "Hello World!";
+        }
+
+        private static string GetGoodbyeMessage()
+        {
+            return "Bye-bye world";
+        }
+
+        private static void Method()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
