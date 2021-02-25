f5550eee64f5add532935e60aea7c4542e77adcd
Sergey Zhizhko <sergey.zhizhko@dataart.com>
2021-02-25 23:17:12 +0200

    add datetime, timespan, string examples

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -2,6 +2,10 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
 using Collections;
 using SampleApp.Logic;
 
@@ -11,12 +15,60 @@ namespace SampleApp
     {
         private static void Main(string[] args)
         {
-            List<int> list = new List<int>();
+            var difference = DateTime.Now - DateTime.Today;
+            Console.WriteLine(difference);
+            
+            Console.WriteLine(DateTime.Now);
+            Thread.Sleep(TimeSpan.FromMinutes(5));
+            Console.WriteLine(DateTime.Now);
 
-            foreach (int value in list)
+            //Console.WriteLine("abc".Equals(string.Empty, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        private static void ValueReferenceTypeExample()
+        {
+            var x = 1;
+            var y = 1;
+
+            var p1 = new Person();
+            var p2 = new Person();
+
+            Console.WriteLine(x == y);
+            Console.WriteLine(p1.Equals(p2));
+        }
+
+        private static void StringExample()
+        {
+            Console.WriteLine(string.Empty);
+            Console.WriteLine($"{string.Empty}");
+            Console.WriteLine(new string('*', 10));
+
+            var numbers = new List<int> { 2, 3, 5, 8 };
+
+            Console.WriteLine(string.Join(", ", numbers));
+
+            var sb = new StringBuilder();
+
+            foreach (var number in numbers)
             {
-                Console.WriteLine(value);
+                sb.Append($"{number}, ");
             }
+
+            var result = sb.ToString();
+        }
+
+        private int GetFromConsole()
+        {
+            // TODO: make utility code from this
+            //Console.WriteLine("please enter a number: ");
+            //var numberStr = Console.ReadLine();
+
+            //if (string.IsNullOrWhiteSpace(numberStr) 
+            //    || !int.TryParse(numberStr, out var number))
+            //{
+            //    return;
+            //}
+            throw new NotImplementedException();
         }
     }
 }
