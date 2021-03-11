023de912eb04ce73d2350e1de935f7654f00d54f
Sergey Zhizhko <sergey.zhizhko@dataart.com>
2021-03-11 21:24:26 +0200

    add settings example

---
+++ Settings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+
+namespace Settings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SetupConsoleColor();
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void SetupConsoleColor()
+        {
+            var configColor = ConfigurationManager.AppSettings["ConsoleColor"];
+            
+            if (!string.IsNullOrWhiteSpace(configColor) && Enum.TryParse<ConsoleColor>(configColor, out var color))
+            {
+                Console.ForegroundColor = color;
+            }
+        }
+    }
+}
