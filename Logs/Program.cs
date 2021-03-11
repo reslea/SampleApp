40ae799cc48616a52559c5d886aeef86f777aa3b
Sergey Zhizhko <sergey.zhizhko@dataart.com>
2021-03-11 21:24:00 +0200

    add logs sample

---
+++ Logs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Logs
+{
+    class Program
+    {
+        private static readonly Guid UniqueIdentifier = Guid.NewGuid();
+
+        static void Main(string[] args)
+        {
+            WriteLog("A number was asked to square");
+            if (int.TryParse(Console.ReadLine(), out var number))
+            {
+                WriteLog($"the result is {Math.Pow(int.MaxValue, 2)}");
+            }
+            else
+            {
+                WriteLog("its not a number");
+            }
+        }
+
+        private static void WriteLog(string message)
+        {
+            Console.WriteLine($"{UniqueIdentifier}: {message}");
+        }
+    }
+}
