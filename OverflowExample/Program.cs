02e675c60bd18fb1ab96ef7c7f879d887ef6a82f
Sergey Zhizhko <sergey.zhizhko@dataart.com>
2021-03-11 21:24:15 +0200

    add overflow example

---
+++ OverflowExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OverflowExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = int.MaxValue;
+
+            checked
+            {
+                Console.WriteLine(i++);
+            }
+
+            Console.WriteLine(i);
+        }
+    }
+}
