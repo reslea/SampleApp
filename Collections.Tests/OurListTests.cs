f5437563c5a1ed428a38f32d3f16f71bb11a42fe
Sergey Zhizhko <sergey.zhizhko@dataart.com>
2021-03-16 22:20:03 +0200

    add OurList tests

--- Collections.Tests/OurListTests.cs
+++ Collections.Tests/OurListTests.cs
@@ -6,9 +6,22 @@ namespace Collections.Tests
     public class OurListTests
     {
         [Fact]
-        public void Test1()
+        public void Add_Test()
         {
+            var numberToAdd = 1;
+            var ourList = new OurList<int>();
 
+            ourList.Add(numberToAdd);
+
+            Assert.Contains(numberToAdd, ourList);
+        }
+
+        [Fact]
+        public void Contains_Test()
+        {
+            var ourList = new OurList<int> { 5 };
+            var result = ourList.Contains(5);
+            Assert.True(result);
         }
     }
 }
