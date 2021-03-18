0d450d7f5506432e09c84f273b69bbd5219d3c0c
Sergey Zhizhko <sergey.zhizhko@dataart.com>
2021-03-18 20:59:57 +0200

    add validation sample

---
+++ Validation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace Validation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person { Email = "1"};
+
+            var type = person.GetType();
+
+            foreach (var property in type.GetProperties())
+            {
+                var attributes = property.CustomAttributes;
+
+                foreach (var attribute in attributes)
+                {
+                    var attributeType = attribute.AttributeType;
+                    var attributeInstance = Activator.CreateInstance(attributeType);
+
+                    if (attributeInstance is ValidationAttribute validationAttribute)
+                    {
+                        var validationResult = validationAttribute.IsValid(property.GetValue(person));
+
+                        Console.WriteLine($"{property.Name}[{attribute}]:{validationResult}");
+                    }
+                }
+            }
+        }
+    }
+
+    public class Person
+    {
+        public int Age { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+    }
+}
