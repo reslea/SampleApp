36eb0f8c01e8d22961b4a9f42c113591d6beaddd
Sergey Zhizhko <sergey.zhizhko@dataart.com>
2021-03-22 21:41:20 +0200

    add data request sample

---
+++ DataRequestSample/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Dynamic;
+
+namespace DataRequestSample
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int PagesCount { get; set; }
+
+        public DateTime PublishDate { get; set; }
+    }
+}
---
+++ DataRequestSample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace DataRequestSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Library;Integrated Security=True;";
+
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            var command = new SqlCommand("SELECT * FROM Books", connection);
+
+            using var reader = command.ExecuteReader();
+
+            var books = GetBooks(reader);
+
+            foreach (var book in books)
+            {
+                Console.WriteLine($"{book.Id} {book.Author} {book.PagesCount}");
+            }
+        }
+
+        public static IEnumerable<Book> GetBooks(SqlDataReader reader)
+        {
+            while (reader.Read())
+            {
+                yield return new Book
+                {
+                    Id = (int)reader[0],
+                    Title = (string)reader[1],
+                    Author = (string)reader[2],
+                    PagesCount = (int)reader[3],
+                    PublishDate = (DateTime)reader[4]
+                };
+            }
+        }
+    }
+}
