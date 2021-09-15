ca449bc9f14a6b5759295e89cd2d58107d67cd56
Serhii Zhyzhko <11r3start11@gmail.com>
2021-09-15 15:43:48 +0300

    Create projectionExample.js

---
+++ EventStoreAccount/projectionExample.js
@@ -0,0 +1,10 @@
+var incomeHandler = (state, evt) => { state.balance += evt.data.sum; };
+var outcomeHandler = (state, evt) => { state.balance -= evt.data.sum; };
+
+fromStream('card-75d7486b-fbbe-4b08-8aa9-bbbbbbbbbbbb')
+	.when({
+		$init: () => ({ balance: 0 }),
+		Receipt: incomeHandler,
+		Withdrawal: outcomeHandler
+	})
+.outputState();
